37b896aee0f28d8db62a0b2da5ae53d03ec955c2
amiyaranjansahoo <meamiyaranjan@gmail.com>
2023-09-12 14:19:11 +0530

    Update max-pods-calculator.sh

--- installation/max-pods-calculator.sh
+++ installation/max-pods-calculator.sh
@@ -3,4 +3,5 @@ curl -O https://raw.githubusercontent.com/awslabs/amazon-eks-ami/master/files/ma
 chmod +x max-pods-calculator.sh
 # Create an user with admin access and configure as below and tehn execute the script
 aws cpnfigure
+./max-pods-calculator.sh --instance-type <instance type> --cni-version <cni version>
 ./max-pods-calculator.sh --instance-type m5.large --cni-version 1.9.0-eksbuild.1
